b75b32d4e420fab3fa418dc1ae828e6114a7bfba
nidhinnidhin <nidhinbabu171@gmail.com>
2025-05-18 22:23:49 +0530

    new

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,9 @@ export interface Project {
   title: string;
   description: string;
   technologies: string[];
-  githubLink: string;
+  githubLink?: string; 
+  githubLink1?: string; 
+  githubLink2?: string;
   demoLink: string;
   image: string;
 }
